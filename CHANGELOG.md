3782259ce9a2646bf53d0ad0523d22a89beae98d
Jamie White <jamie@jgwhite.co.uk>
2014-02-20 15:04:01 +0000

    v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # master
 
+# 0.0.2
+
+* Update for Broccoli 0.2.0
+
 # 0.0.1
 
 * Initial release
--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-module.exports = function (broccoli) {
-  var fs = require('fs')
-  var path = require('path')
-  var sass = require('node-sass')
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
+var sass = require('node-sass')
+var broccoli = require('broccoli')
+var Transform = require('broccoli-transform')
 
-  SassCompiler.prototype = Object.create(broccoli.Compiler.prototype)
-  SassCompiler.prototype.constructor = SassCompiler
-  function SassCompiler(options) {
-    this.options = options || {}
+module.exports = SassCompiler
+SassCompiler.prototype = Object.create(Transform.prototype)
+SassCompiler.prototype.constructor = SassCompiler
+function SassCompiler(inputTree, options) {
+  if (!(this instanceof SassCompiler)) return new SassCompiler(inputTree, options)
+  this.inputTree = inputTree
+  for (var key in options) {
+    if (options.hasOwnProperty(key)) {
+      this[key] = options[key]
+    }
   }
 
-  SassCompiler.prototype.compile = function(srcDir, destDir) {
-    var inputFile = path.join(srcDir, this.options.inputFile)
-    var outputFile = path.join(destDir, this.options.outputFile)
+  this.cache = {}
+}
 
-    var css = sass.renderSync({
-      file: inputFile,
-      outputStyle: this.options.outputStyle || 'compressed'
-    })
+SassCompiler.prototype.transform = function (srcDir, destDir) {
+  var inputFile = path.join(srcDir, this.inputFile)
+  var outputFile = path.join(destDir, this.outputFile)
 
-    fs.writeFileSync(outputFile, css)
-  }
+  var css = sass.renderSync({
+    file: inputFile,
+    outputStyle: this.outputStyle || 'compressed'
+  })
 
-  return SassCompiler
+  broccoli.helpers.assertAbsolutePaths([this.outputFile])
+  mkdirp.sync(path.join(destDir, path.dirname(this.outputFile)))
+  fs.writeFileSync(outputFile, css)
 }
